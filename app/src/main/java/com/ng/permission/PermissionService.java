606f31886672f6dd7d6cbdb79a8fba01750b2221
Nitin <nitin.gupta0805@gmail.com>
2020-06-22 06:37:32 -0700

    added new service

---
+++ app/src/main/java/com/ng/permission/PermissionService.java
@@ -0,0 +1,16 @@
+package com.ng.permission;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class PermissionService extends Service {
+    public PermissionService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
